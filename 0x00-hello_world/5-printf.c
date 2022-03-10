cc430b8aa5293a1654a10de0b20c0d471b64907e
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-10 23:03:37 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
-#include<stdio>
+#include <stdio.h>
 /**
- * print a adata using printf 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
