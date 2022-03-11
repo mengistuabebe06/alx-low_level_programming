63e18733ae8624c4090d9abf2e6e4e4b1776ece5
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-12 00:02:53 +0300

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,20 @@
 /**
  *  *main-program entry point.
  *   *Return:0 if no error, non-zero value if error.
- *    */
+ */
 int main(void)
 {
-char i;
-char j;
-for (i = 'a'; i <= 'z'; i++)
-{	putchar(i);
-}for (j = 'A'; j <= 'Z'; j++)
-{
-putchar(j);
-}
-putchar('\n');
-return (0);
+	char i;
+	char j;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{	
+		putchar(j);
+	}	
+	
+	putchar('\n');
+	return (0);
 }
