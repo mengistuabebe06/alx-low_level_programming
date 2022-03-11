ab801a5aaaacb85ab638ddde73a4cd88bfe96b03
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-12 01:08:35 +0300

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,8 +5,7 @@ int main(void)
 	int c;
 	int d;
 	int e;
-	int f=0;
-	
+	int f = 0;
 	while (f < 10) 
 	{
 	e = 0;
@@ -18,14 +17,14 @@ int main(void)
 			c = 0;
 			while (c < 10)
 			{
-				if (!( f==c && e == d))
+				if (!(f == c && e == d))
 				{
 					putchar('0' + f);
 					putchar('0' + e);
 					putchar(' ');
 					putchar('0' + d);
 					putchar('0' + c);
-					if(!(f + e == 18 && c + d ==17 && d == 9))
+					if (!(f + e == 18 && c + d == 17 && d == 9))
 					{	
 						putchar(',');
 						putchar(' ');
