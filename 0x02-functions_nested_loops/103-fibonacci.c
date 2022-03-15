fe68c3f99e0f1d72d9f2504453ca9aa530ae782a
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-16 00:54:10 +0300

    q00

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,8 +3,7 @@
  * main - main block
  * Description: computes and prints even  number < 4,000,000
  * 5 below 1024 (excluded), followed by a new line
- * Return: 0
- * 
+ * Return: 0 
  */
 
 int main(void)
