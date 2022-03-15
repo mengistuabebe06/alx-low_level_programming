894a53d8fc9127878476f9a1cbc5f5492f20523e
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-15 22:50:40 +0300

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,8 +13,7 @@ void print_alphabet_x10(void)
 	while (co < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
-		{
-																		_putchar(n);
+		{																_putchar(n);
 																	}
 		co++;
 		_putchar('\n');
