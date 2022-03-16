18e5a8b1e768ff6e1b480e042bff222e6e73a2f5
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-16 17:13:31 +0300

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,44 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+  * times_table - prints times table
+  * Return:void
+  *.
  */
 
-int main(void)
+void times_table(void)
 {
-	 times_table();
- 	 return (0);
+	int a = 0;
+	int b;
+	int rep;
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			rep = a * b;
+			if (b == 0)
+			{
+				_putchar('0' + rep);
+			}
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_puchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
 }
