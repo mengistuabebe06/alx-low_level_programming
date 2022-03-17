8fb6a9c06ff4b7cf8219cf9478b78e52c5540539
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-17 23:16:08 +0300

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+
 /**
- *  * main - prime numbers
- *   *
- *    * Return: Always 0.
- *     */
+ * main - prime numbers
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	unsigned int long n = 612852475143, a = (int) sqrt(n);
