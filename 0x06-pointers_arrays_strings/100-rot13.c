c7549ae2149e30b54376dbd57dd557f0651603c8
mengistuabebe06 <ethiomengistu@gmail.com>
2022-03-24 21:42:09 +0300

    rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,14 +14,13 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (ii = 0; ii < 54; ii++;)
+		for (ii = 0; ii < 54; ii++)
 		{
-			if ((s[i] <= 'z' && s[i] >= 'a' || (s[i] <= 'Z' && s[i] >= 'A'))
+			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
 			&& s[i] == input[ii])
 			{
 				s[i] = output[ii];
 				break;
-			
 			}
 		}
 	}
